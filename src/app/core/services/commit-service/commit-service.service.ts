b63a6ddbb9474a468172cf1db80da925470f0dc0
Ayodele Opetumo <aopetumo@gmail.com>
2023-02-06 15:01:53 +0100

    include pagination in commit service

--- src/app/core/services/commit-service/commit-service.service.ts
+++ src/app/core/services/commit-service/commit-service.service.ts
@@ -12,8 +12,8 @@ export class CommitService {
 
   constructor(private http: HttpClient) { }
 
-  getCommits(since: string, until: string): Observable<CommitResponse[]>{
-    return this.http.get<CommitResponse[]>(`${this.apiUrl}?since=${since}&until=${until}&per_page=50`);
+  getCommits(since: string, until: string, page: number = 1): Observable<CommitResponse[]>{
+    return this.http.get<CommitResponse[]>(`${this.apiUrl}?since=${since}&until=${until}&page=${page}`);
   }
 
   getCommitDetails(commitHash: string): Observable<CommitDetailsResponse> {
